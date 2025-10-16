8dc32504082a760f97dd0342adc21112a549f496
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-10-16 16:58:47 -0600

    added dataset para yolo

--- pruebas/YOLO-train/download-dataset.py
+++ pruebas/YOLO-train/download-dataset.py
@@ -1,5 +1,7 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="your_API_key")
+import os
+rf = Roboflow(api_key="X8dcp78gfishEDl9jWt5")
 project = rf.workspace("viren-dhanwani").project("tennis-ball-detection")
 version = project.version(6)
+
 dataset = version.download("yolov8")
