b659906e1b35cfafce4e780b7ad31d8319d61c18
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-10-16 19:54:29 -0600

    delete dataset que no nos iba a jalar

--- pruebas/YOLO-train/download-dataset.py
+++ pruebas/YOLO-train/download-dataset.py
@@ -1,7 +1,7 @@
 from roboflow import Roboflow
 import os
 rf = Roboflow(api_key="X8dcp78gfishEDl9jWt5")
-project = rf.workspace("viren-dhanwani").project("tennis-ball-detection")
-version = project.version(6)
+project = rf.workspace("joses-workspace-bfegj").project("tennis-balls")
+version = project.version(1)
 
 dataset = version.download("yolov8")
