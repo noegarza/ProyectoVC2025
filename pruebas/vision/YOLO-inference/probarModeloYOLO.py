0167dc4c8f0831dedef0098d2044cc5b30be6a31
rpi vc repo <jose.amarob@udem.edu>
2025-10-18 13:54:39 -0600

    .

--- pruebas/vision/YOLO-inference/probarModeloYOLO.py
+++ pruebas/vision/YOLO-inference/probarModeloYOLO.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 import cv2
 
 # Ruta al modelo entrenado
-model_path = "modelos-YOLO/v8n-e10-imgsz320/weights/best.pt"
+model_path = "modelos-YOLO/v8n-e20-imgsz80/weights/best.pt"
 model = YOLO(model_path)
 
 
