6a8b56965c70ccbdf52c8c66c9a9537227f0c53e
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-10-20 14:03:27 -0600

    .

--- pruebas/comunicacion-sockets/basic/cl.py
+++ pruebas/comunicacion-sockets/basic/cl.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import pickle
 import struct
+import numpy as np
 import time
 import cv2
 import traceback
@@ -19,3 +20,35 @@ temp_data     = []
 saving_active = False
 payload_size  = struct.calcsize("Q")
 data_buffer   = b""
+
+
+# — Hilo de vídeo —
+def recv_video():
+    global data_buffer, streaming
+    while True:
+        if not streaming:
+            time.sleep(0.1)
+            continue
+
+        # leer tamaño
+        while len(data_buffer) < payload_size:
+            packet = vid_sock.recv(4096)
+            if not packet:
+                return
+            data_buffer += packet
+        frame_size = struct.unpack("Q", data_buffer[:payload_size])[0]
+        data_buffer = data_buffer[payload_size:]
+
+        # leer frame
+        while len(data_buffer) < frame_size:
+            data_buffer += vid_sock.recv(4096)
+        frame_data = data_buffer[:frame_size]
+        data_buffer = data_buffer[frame_size:]
+
+        # reconstruir y mostrar
+        img_array = np.frombuffer(frame_data, dtype=np.uint8)
+        frame     = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        cv2.imshow("Stream RPi", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            streaming = False
+            cv2.destroyAllWindows()
--- pruebas/comunicacion-sockets/basic/srv.py
+++ pruebas/comunicacion-sockets/basic/srv.py
@@ -17,15 +17,32 @@ server_run   = True
 cam          = None
 
 # — Configuración de sockets —
-HOST       = '172.32.172.199'
+HOST       = '0.0.0.0'
 VIDEO_PORT = 50000
 
-srv_vid  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srv_vid .setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+srv_vid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv_vid.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-srv_vid .bind((HOST, VIDEO_PORT))
+srv_vid.bind((HOST, VIDEO_PORT))
 srv_vid.listen(1)
 print(f"[SERVIDOR] Video en {HOST}:{VIDEO_PORT}")
 
 conn_vid,  addr_v = srv_vid .accept()
 print(f"[SERVIDOR] Video conectado:   {addr_v}")
+
+
+# — Hilo de vídeo —
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    else:
+        _, encimg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+        data = encimg.tobytes()
+        conn_vid.sendall(struct.pack("Q", len(data)) + data)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            break
+    
