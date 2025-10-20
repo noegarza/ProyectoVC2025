b3d81b2729d6e8d249e07f9fa0e1acefdef175cc
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-10-20 13:47:57 -0600

    .

--- pruebas/comunicacion-sockets/basic/cl.py
+++ pruebas/comunicacion-sockets/basic/cl.py
@@ -6,3 +6,16 @@ import struct
 import time
 import cv2
 import traceback
+
+HOST       = ''
+VIDEO_PORT = 50000
+
+vid_sock  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+vid_sock.connect((HOST, VIDEO_PORT))
+
+# — Variables globales —
+streaming     = False
+temp_data     = []
+saving_active = False
+payload_size  = struct.calcsize("Q")
+data_buffer   = b""
