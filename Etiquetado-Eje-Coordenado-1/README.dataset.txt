fff0e14a4826a2a5b1388d6f2e18b8bf09fa5667
Alejandro-LozanoV <alejandro.lozanov@udem.edu>
2025-11-01 10:50:41 -0600

    popo_69_420

---
+++ Etiquetado-Eje-Coordenado-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# Etiquetado Eje Coordenado > v1
+https://universe.roboflow.com/alex4/etiquetado-eje-coordenado-muxaf
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- pruebas/vision/YOLO-train/download-dataset.py
+++ pruebas/vision/YOLO-train/download-dataset.py
@@ -1,7 +1,7 @@
 from roboflow import Roboflow
 import os
 rf = Roboflow(api_key="")
-project = rf.workspace("amaro27").project("tennis-balls-ciwdv")
+project = rf.workspace("alex4").project("etiquetado-eje-coordenado-muxaf")
 version = project.version(1)
 
 dataset = version.download("yolov8")
