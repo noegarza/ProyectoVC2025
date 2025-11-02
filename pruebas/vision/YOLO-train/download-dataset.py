04c9ced84562c8e7a0f4197cc7a9d174cfdaa6a3
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-11-02 09:05:56 -0600

    se agregó dataset completo

--- pruebas/vision/YOLO-train/download-dataset.py
+++ pruebas/vision/YOLO-train/download-dataset.py
@@ -1,7 +1,7 @@
 from roboflow import Roboflow
 import os
-rf = Roboflow(api_key="")
+rf = Roboflow(api_key="X8dcp78gfishEDl9jWt5")
 project = rf.workspace("amaro27").project("v2-axis-haptic-dino-x8w6i")
 version = project.version(1)
 
-dataset = version.download("yolov8", location="datasets")
+dataset = version.download("yolov8")
