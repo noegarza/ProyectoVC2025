8456f0757ce4a89f05f1f41830d48a1c6d39d9fd
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-11-03 16:53:50 -0600

    added dataset augmented rgb gray y entrenar modelo

--- pruebas/vision/YOLO-inference/probarModeloYOLO/probarModeloYOLO.py
+++ pruebas/vision/YOLO-inference/probarModeloYOLO/probarModeloYOLO.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import numpy as np
 
 # Parámetros YOLO
-model_path = "modelos-YOLO-parcial3/v8n-640-50-aug-rgb-3/weights/best.pt"
+model_path = "modelos-YOLO-parcial3/v8n-640-50-aug-rgb-gray-2/weights/best.pt"
 model = YOLO(model_path)
 
 # Parámetros de cálculo de FPS
