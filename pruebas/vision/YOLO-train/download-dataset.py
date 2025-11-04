fdfee9b8b660b58202ee2f8359cb694dd0399cca
José Ángel Amaro Blanco <jose.amarob@udem.edu>
2025-11-04 10:33:43 -0600

    train con rebuild

--- pruebas/vision/YOLO-train/download-dataset.py
+++ pruebas/vision/YOLO-train/download-dataset.py
@@ -2,6 +2,6 @@ from roboflow import Roboflow
 import os
 rf = Roboflow(api_key="")
 project = rf.workspace("amaro27").project("v2-axis-haptic-dino-x8w6i")
-version = project.version(2)
+version = project.version(7)
 
 dataset = version.download("yolov8")
