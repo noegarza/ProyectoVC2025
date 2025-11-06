71363aeb1b8f2ae2ad6a3bda9e160a36fe627295
rpi vc repo <jose.amarob@udem.edu>
2025-11-06 17:15:52 -0600

    .

--- pruebas/vision/YOLO-inference/probarModeloYOLO/probarModeloYOLO.py
+++ pruebas/vision/YOLO-inference/probarModeloYOLO/probarModeloYOLO.py
@@ -13,7 +13,7 @@ model_path = "modelos-YOLO-parcial3/v8n-640-50-aug-rgb-gray-reb-blur-2/weights/b
 model = YOLO(model_path)
 
 # Parámetros de cálculo de FPS
-USANDO_YOLO = False
+USANDO_YOLO = True
 prevFrameTime = time.time()
 newFrameTime = 0
 ctr = 0; allFPS = []
